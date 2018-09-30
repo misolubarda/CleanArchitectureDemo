4cf0647fc83578da3e74cf9e0b5a522d1c7f9c7a
Miso Lubarda <miso.lubarda@here.com>
2018-09-30 09:26:10 +0200

    Add Domain and Data layer to the project
    As per clean architecture, Domain layer will host business logic while
    Data layer will host non-UI service/platform specific logic of the
    application.

---
+++ DataLayer/DataLayer/TestFile.swift
@@ -0,0 +1,13 @@
+//
+//  TestFile.swift
+//  DataLayer
+//
+//  Created by Lubarda, Miso on 9/30/18.
+//  Copyright © 2018 Lubarda, Miso. All rights reserved.
+//
+
+import Foundation
+
+class TestFile {
+    
+}
---
+++ DomainLayer/DomainLayer/TestFile.swift
@@ -0,0 +1,12 @@
+//
+//  TestFile.swift
+//  DomainLayer
+//
+//  Created by Lubarda, Miso on 9/30/18.
+//  Copyright © 2018 Lubarda, Miso. All rights reserved.
+//
+
+import Foundation
+
+class TestFile {
+}
