e7d4ca0be2c75ead938d2bf44279a669a16056dd
Miso Lubarda <miso.lubarda@here.com>
2018-09-30 09:29:48 +0200

    Adding Movie as a business logic entity.

---
+++ DomainLayer/DomainLayer/Entities/Movie.swift
@@ -0,0 +1,16 @@
+//
+//  Movie.swift
+//  DomainLayer
+//
+//  Created by Lubarda, Miso on 9/30/18.
+//  Copyright © 2018 Lubarda, Miso. All rights reserved.
+//
+
+import Foundation
+
+public struct Movie {
+    public let posterPath: String
+    public let title: String
+    public let release: Date
+    public let overview: String
+}
