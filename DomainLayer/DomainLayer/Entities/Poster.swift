35e842db6ddce1f9fef4a8528b640dc6d993cc3a
Miso Lubarda <miso.lubarda@here.com>
2018-10-06 15:00:39 +0200

    Create Poster entity

---
+++ DomainLayer/DomainLayer/Entities/Poster.swift
@@ -0,0 +1,14 @@
+//
+//  Poster.swift
+//  DomainLayer
+//
+//  Created by Lubarda, Miso on 06.10.18.
+//  Copyright © 2018 Lubarda, Miso. All rights reserved.
+//
+
+import Foundation
+
+public struct Poster {
+    let path: String
+    let image: Data
+}
